7bd3b719d151ee8613bab2c2b658e241339fe841
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2025-10-30 14:37:34 +0100

    add Moster class with properties and constructor

---
+++ src/moster.ts
@@ -0,0 +1,12 @@
+class Moster {
+    name: string;
+    age: number;
+    hobbies: string[];
+
+
+    constructor(name: string, age: number, hobbies: string[]) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+}
