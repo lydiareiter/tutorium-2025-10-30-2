251ddd28c170d41c9df19bf331186759bcbfcbad
lydiareiter <l.maslowski@students.htl-leonding.ac.at>
2025-10-30 15:04:17 +0100

    refactor: encapsulate properties in Monster class

--- src/monster.ts
+++ src/monster.ts
@@ -1,16 +1,30 @@
 export class Monster {
-    name: string;
-    age: number;
-    hobbies: string[];
+    private _name: string;
+    private _age: number;
+    private _hobbies: string[];
 
 
     constructor(name: string, age: number, hobbies: string[]) {
-        this.name = name;
-        this.age = age;
-        this.hobbies = hobbies;
+        this._name = name;
+        this._age = age;
+        this._hobbies = hobbies;
     }
 
     addHobby (hobby: string) {
-        this.hobbies.push(hobby);
+        this._hobbies.push(hobby);
+    }
+
+
+    get name(): string {
+        return this._name;
+    }
+
+
+    get age(): number {
+        return this._age;
+    }
+
+    get hobbies(): string[] {
+        return this._hobbies;
     }
 }
